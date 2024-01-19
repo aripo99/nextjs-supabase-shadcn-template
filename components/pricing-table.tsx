a5189faf7d450bca4ace00f019d9c3d0167a29c3
aripo99 <a_polakof@umail.ucsb.edu>
2024-01-19 07:47:18 -0800

    Add subscription logic

--- components/pricing-table.tsx
+++ components/pricing-table.tsx
@@ -3,12 +3,14 @@
 import { CheckCircleIcon } from 'lucide-react';
 import { useState } from 'react';
 
-import plans from '@/lib/stripe/plans';
-import { Button } from '@/components/ui/button';
+import plans from '../lib/stripe/plans';
+import { Button } from './ui/button';
+import { createCheckoutAction } from '@/lib/actions/subscription';
 
 function PricingTable() {
-    const [selectedCycle, setSelectedCycle]
-        = useState<'monthly' | 'yearly'>('monthly');
+    const [selectedCycle, setSelectedCycle] = useState<'monthly' | 'yearly'>(
+        'monthly',
+    );
 
     const getVariant = (cycle: 'monthly' | 'yearly') => {
         if (cycle === selectedCycle) {
@@ -19,11 +21,11 @@ function PricingTable() {
     };
 
     return (
-        <div className='flex flex-col space-y-8 items-center'>
-            <div className='flex justify-center'>
-                <div className='flex space-x-1'>
+        <div className="flex flex-col space-y-8 items-center">
+            <div className="flex justify-center">
+                <div className="flex space-x-1">
                     <Button
-                        type='button'
+                        type="button"
                         onClick={() => setSelectedCycle('monthly')}
                         variant={getVariant('monthly')}
                     >
@@ -31,7 +33,7 @@ function PricingTable() {
                     </Button>
 
                     <Button
-                        type='button'
+                        type="button"
                         onClick={() => setSelectedCycle('yearly')}
                         variant={getVariant('yearly')}
                     >
@@ -40,7 +42,7 @@ function PricingTable() {
                 </div>
             </div>
 
-            <div className='flex space-x-4'>
+            <div className="flex space-x-4">
                 {plans.map((plan) => {
                     const selectedPrice = plan.prices.find(({ name }) => {
                         return name.toLowerCase() === selectedCycle;
@@ -55,50 +57,42 @@ function PricingTable() {
                     }
 
                     return (
-                        <form key={selectedPrice.price}>
+                        <form key={selectedPrice.price} action={createCheckoutAction}>
                             <div
-                                className='flex flex-col space-y-4 p-6 rounded-xl border
-                border-gray-100 shadow-sm dark:border-slate-800'
+                                className="flex flex-col space-y-4 p-6 rounded-xl border
+            border-gray-100 shadow-sm dark:border-slate-800"
                             >
-                                <h2 className='font-semibold text-lg'>
-                                    {plan.name}
-                                </h2>
+                                <h2 className="font-semibold text-lg">{plan.name}</h2>
 
-                                <div className='flex items-center space-x-1'>
-                                    <span className='text-2xl font-bold'>
+                                <div className="flex items-center space-x-1">
+                                    <span className="text-2xl font-bold">
                                         ${selectedPrice.price}
                                     </span>
-                                    <span className='lowercase text-sm'>
+                                    <span className="lowercase text-sm">
                                         /{selectedPrice.name}
                                     </span>
                                 </div>
 
-                                <div className='flex flex-col space-y-1'>
+                                <div className="flex flex-col space-y-1">
                                     {plan.features.map((feature) => {
                                         return (
                                             <div
                                                 key={feature}
-                                                className='flex space-x-2.5 items-center text-sm'
+                                                className="flex space-x-2.5 items-center text-sm"
                                             >
-                                                <CheckCircleIcon className='w-4 h-4 text-green-500' />
+                                                <CheckCircleIcon className="w-4 h-4 text-green-500" />
                                                 <span>{feature}</span>
                                             </div>
-                                        )
+                                        );
                                     })}
                                 </div>
 
-                                <input
-                                    type='hidden'
-                                    name='priceId'
-                                    value={selectedPrice?.id}
-                                />
+                                <input type="hidden" name="priceId" value={selectedPrice?.id} />
 
-                                <Button variant={'outline'}>
-                                    Subscribe
-                                </Button>
+                                <Button variant={'outline'}>Subscribe</Button>
                             </div>
                         </form>
-                    )
+                    );
                 })}
             </div>
         </div>
